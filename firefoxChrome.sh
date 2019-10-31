6ac879e7eb89fb1bc9a3223f4637ee26e7a1de58
jokersus <lolivampireslave@gmail.com>
2019-10-31 17:29:41 +0900

    Changed userCrome.css path

--- firefoxChrome.sh
+++ firefoxChrome.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cat ~/.cache/wal/colors.css ~/.config/firefoxTheme.css > ~/.mozilla/firefox/x17mhv47.default/chrome/userChrome.css
+cat ~/.cache/wal/colors.css ~/.config/firefoxTheme.css > ~/.mozilla/firefox/sgvje7ih.default-release/chrome/userChrome.css
