c99931b51f93b7332e092bfb95387a3d0cc40ea9
jokersus <lolivampireslave@gmail.com>
2020-06-07 01:51:34 +0400

    Added bindings for ctrl + backspace, but it doesn't work

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,10 @@
 # wal -e -i /home/jokersus/Pictures/wallhaven-8396gk.jpg > /dev/null 2>&1
 # cat ~/.cache/wal/sequences &
 
+# sanity stuff
 set fish_greeting
+bind \e\x7F 'backward-kill-word'
+bind \e\b 'backward-kill-word'
 
 # aliases
 alias meow "cat"
