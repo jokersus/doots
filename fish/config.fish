3bbeae249ca35f81de8065162253f2c738ad1f2c
jokersus <lolivampireslave@gmail.com>
2021-09-09 14:36:44 +0400

    Removed yay alias

--- fish/config.fish
+++ fish/config.fish
@@ -12,7 +12,6 @@ set -g theme_color_scheme terminal2
 set TERM xterm
 
 # aliases
-alias yay "yay --editmenu"
 alias meow "cat"
 alias paclast "expac --timefmt='%Y-%m-%d %T' '%l\t%n' | sort | tail -n "
 alias l "ls -lha"
