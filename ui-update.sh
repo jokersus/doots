64f6710a67311129fc74255e31829f74487230b7
jokersus <lolivampireslave@gmail.com>
2021-11-09 10:39:49 +0300

    Changed wal backend to haishoku

--- ui-update.sh
+++ ui-update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wal -a 90 -i ~/Pictures/Wallpapers/random
+wal -a 90 -i ~/Pictures/Wallpapers/random --backend haishoku
 
 ~/.config/polybar/polybar.sh &
 dunst -conf ~/.config/dunst/dunstrc_xr_colors &
