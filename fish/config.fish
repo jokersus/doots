630c34daf5901d85249224664ba6d6ec6236edeb
jokersus <lolivampireslave@gmail.com>
2022-09-20 16:26:10 +0400

    Added neovim

--- fish/config.fish
+++ fish/config.fish
@@ -10,7 +10,7 @@ bind \e\b 'backward-kill-word'
 set -g theme_show_exit_status yes
 set -g theme_color_scheme terminal2
 #set TERM xterm
-set TERM xterm-256color
+set TERM screen-256color
 
 # aliases
 alias meow "cat"
@@ -23,10 +23,11 @@ alias cp "rsync -azhvP"
 # ranger cd
 # See https://github.com/limakzi/fisher-ranger-cd
 # alias ranger "__ranger_cd"
-alias ranger='ranger --choosedir=/tmp/.rangerdir; cd (cat /tmp/.rangerdir)'
+alias ranger 'ranger --choosedir=/tmp/.rangerdir; cd (cat /tmp/.rangerdir)'
+alias vim nvim
 
 # export
-export EDITOR=vim
+export EDITOR=nvim
 export TERMINAL=urxvtc
 
 # This is magic
